5a25b02f4928a236099a5ceeb98ffb091aca5edf
johnnycchapman <chapmancjohnny@gmail>
2022-02-23 16:54:54 -0500

    add github code

---
+++ Github-Add-Script.ps1
@@ -0,0 +1,16 @@
+﻿#https://education.github.com/git-cheat-sheet-education.pdf
+#git config –global user.email "chapmancjohnny@gmail.com"
+git remote add origin https://github.com/johnnycchapman/Powershell.git
+git add "C:\Users\Johnny C\Desktop\Projects\Kubernetes\Install-Chocolatey.ps1"
+git commit -m "add choco script"
+git push -u origin main
+
+<#
+echo "# Powershell" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/johnnycchapman/Powershell.git
+git push -u origin main
+#>
