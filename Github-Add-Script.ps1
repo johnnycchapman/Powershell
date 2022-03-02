c1f5f8b8922ee5a8c459af915f93c66fa1e81504
johnnycchapman <chapmancjohnny@gmail>
2022-03-02 11:59:09 -0500

    remove user info

--- Github-Add-Script.ps1
+++ Github-Add-Script.ps1
@@ -1,8 +1,7 @@
 ﻿#https://education.github.com/git-cheat-sheet-education.pdf
-#git config –global user.email "chapmancjohnny@gmail.com"
-git remote add origin https://github.com/johnnycchapman/Powershell.git
-git add "C:\Users\Johnny C\Desktop\Projects\Kubernetes\Install-Chocolatey.ps1"
-git commit -m "add choco script"
+git remote add origin https://github.com/<username>/<repository>.git
+git add "C:\<file-path>\<filename>"
+git commit -m "add file"
 git push -u origin main
 
 <#
@@ -11,6 +10,6 @@ git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/johnnycchapman/Powershell.git
+git remote add origin https://github.com/<username>/<repository>.git
 git push -u origin main
 #>
